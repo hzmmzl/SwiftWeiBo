63989f2902dd7b7c0ff2e566a72b23c5e9e5e5d5
mzl <2736652044@qq.com>
2017-04-26 17:42:19 +0800

    框架基本搭建

--- TestSwiftWeiBo/TestSwiftWeiBo/AppDelegate.swift
+++ TestSwiftWeiBo/TestSwiftWeiBo/AppDelegate.swift
@@ -15,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+//        window?.backgroundColor = UIColor(red: 0, green: 1, blue: 0.4, alpha: 1)
+        let mainVC = MainTabBarViewController()
+        window?.rootViewController = mainVC
+        
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
